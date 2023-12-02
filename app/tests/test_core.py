4c2f14cd0bcdeebf1247a151bd212c4ca2f381c8
tarunshankarguhacogoport <Tarun.Guha@cogoport.com>
2023-12-02 23:10:21 +0530

    Env Test Case Fix

--- app/tests/test_core.py
+++ app/tests/test_core.py
@@ -10,4 +10,3 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Life Deployed Successfully!"}
